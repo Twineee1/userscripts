23e2fddd0e64cf5ed4027b89a514c9112928d8e2
Twineee <twineee@tutamail.com>
2025-06-05 13:22:05 -0500

    Create AdBlock.js

---
+++ WP/AdBlock.js
@@ -0,0 +1,8 @@
+if (typeof BlockAds1 !== 'undefined') {/*shows by default*/
+$( '.qxz-ads' ).hide();}
+
+if (typeof BlockAds2 === 'undefined') {/*hides by default*/
+$( '.vector-sitenotice-container' ).hide();}
+
+//.vector-sitenotice-container for the big boys and
+//.qxz-ads for the templates
