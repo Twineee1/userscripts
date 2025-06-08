ac6c72c8c1f76951f6ceb7f2a238b98c18b4ccd7
Twineee <twineee@tutamail.com>
2025-06-07 21:25:23 -0500

    Update Old SE Chat.user.js

--- SE/Old SE Chat.user.js
+++ SE/Old SE Chat.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         Old SE chat
 // @namespace    http://tampermonkey.net/
-// @version      2025-06-05
+// @version      2025-06-07
 // @description  try to take over the world with the old chat interface!
 // @author       Twineee
-// @match        https://chat.stackexchange.com/rooms/*
+// @match        https://chat.stackexchange.com/*
+// @match        https://chat.meta.stackexchange.com/*
+// @match        https://chat.stackoverflow.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @require     https://code.jquery.com/jquery-3.6.0.min.js\
+// @require      https://code.jquery.com/jquery-3.6.0.min.js\
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/Twineee1/userscripts/refs/heads/main/SE/Old%20SE%20Chat
 // ==/UserScript==
 
 (function() {
