645b59a2f249d6f784a390012b0b353ca74e70a6
Twineee <twineee@tutamail.com>
2025-07-01 12:33:39 -0500

    Create Thatsthejoke.user.js

---
+++ SE/Thatsthejoke.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         That's The Joke!
+// @namespace    http://tampermonkey.net/
+// @version      2025-07-01
+// @description  try to take over the world with this new button!
+// @author       Twineee
+// @match        https://chat.stackexchange.com/rooms/*
+// @match        https://chat.stackoverflow.com/rooms/*
+// @match        https://chat.meta.stackexchange.com/rooms/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackexchange.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $("#chat-buttons").append("<button class='button' id='thatsdajoke-button'>That's The Joke</button>");
+
+    $("#thatsdajoke-button").click(function() {
+        $("#input").val("https://www.youtube.com/watch?v=5ngg5-uwQYI");
+        $('#sayit-button')[0].click()
+    })
+})();
